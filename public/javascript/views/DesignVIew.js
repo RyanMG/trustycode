0fe7dc5267ecf4e3c13eb256925678f5faa36724
David Ryan Hall <david.ryan.hall@gmail.com>
2013-12-24 22:01:15 -0800

    Scroll left for portfolio

--- public/javascript/views/DesignVIew.js
+++ public/javascript/views/DesignVIew.js
@@ -3,7 +3,7 @@
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-  define(['jquery', 'underscore', 'backbone', 'text!templates/sections/design.html'], function($, _, Backbone, design) {
+  define(['jquery', 'underscore', 'backbone', 'text!templates/sections/design.html'], function($, _, Backbone, design, designPort) {
     var DesignView, _ref;
     DesignView = (function(_super) {
       __extends(DesignView, _super);
@@ -15,9 +15,17 @@
 
       DesignView.prototype.el = $('#design');
 
+      DesignView.prototype.events = {
+        'click #designPortfolio': 'loadDesignPortfolio'
+      };
+
       DesignView.prototype.render = function() {
         this.template = _.template(design);
         return this.$el.html(this.template);
+      };
+
+      DesignView.prototype.loadDesignPortfolio = function() {
+        return this.designPortTemplate = _.template(designPort);
       };
 
       return DesignView;
--- resources/coffeescript/views/DesignVIew.coffee
+++ resources/coffeescript/views/DesignVIew.coffee
@@ -1,9 +1,16 @@
-define(['jquery', 'underscore', 'backbone', 'text!templates/sections/design.html'], ($, _, Backbone, design) =>
+define(['jquery', 'underscore', 'backbone', 'text!templates/sections/design.html'], ($, _, Backbone, design, designPort) =>
   class DesignView extends Backbone.View
-    el: $('#design'),
+    el: $('#design')
+    events:
+      'click #designPortfolio': 'loadDesignPortfolio'
+
     render: ->
       @template = _.template design
       @$el.html(@template)
+
+    loadDesignPortfolio: ->
+      @designPortTemplate = _.template designPort
+
 
   DesignView
 )
